d5d26530d9cdf33fcbc35fc4a5bb2efed9e4f32c
Martyn Loughran <me@mloughran.com>
2011-05-23 13:50:04 +0200

    Print the revision when calling get

--- cmd/doozer/get.go
+++ cmd/doozer/get.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"os"
+	"fmt"
 )
 
 
 func init() {
 	cmds["get"] = cmd{get, "<path>", "read a file"}
-	cmdHelp["get"] = "Prints the body of the file at <path>.\n"
+	cmdHelp["get"] = "Prints the body and revision of the file at <path>.\n"
 }
 
 
 func get(path string) {
 	c := dial()
 
-	body, _, err := c.Get(path, nil)
+	body, actualRev, err := c.Get(path, nil)
 	if err != nil {
 		bail(err)
 	}
 
+	fmt.Println("Revision:", actualRev)
 	os.Stdout.Write(body)
 }
