b85d9688743b76ce83690d612d1d8733931b2bad
Martyn Loughran <me@mloughran.com>
2011-05-23 13:56:22 +0200

    Allow requesting a specific revision with get
    * I haven't worked out how optional arguments should be handled so for now if you don't care about the revision you must call `doozer get file nil`

--- cmd/doozer/get.go
+++ cmd/doozer/get.go
@@ -7,15 +7,27 @@ import (
 
 
 func init() {
-	cmds["get"] = cmd{get, "<path>", "read a file"}
+	cmds["get"] = cmd{get, "<path> <rev|nil>", "read a file"}
 	cmdHelp["get"] = "Prints the body and revision of the file at <path>.\n"
 }
 
 
-func get(path string) {
+func get(path string, rev string) {
 	c := dial()
 
-	body, actualRev, err := c.Get(path, nil)
+	var (
+		body      []byte
+		actualRev int64
+		err       os.Error
+	)
+
+	if rev == "nil" {
+		body, actualRev, err = c.Get(path, nil)
+	} else {
+		requestedRev := mustAtoi64(rev)
+		body, actualRev, err = c.Get(path, &requestedRev)
+	}
+
 	if err != nil {
 		bail(err)
 	}
