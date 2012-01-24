ceb75f64a96818c376f9a22c7f61a513c9feac09
Alexander Simmerl <alx@soundcloud.com>
2012-01-24 15:56:36 +0100

    Reimplement Walk to expect WalkFunc
    * filepath.Walk was recently changed [https://groups.google.com/group/golang-nuts/msg/e304dd9cf196a218]
    * Remove Visitor interface
    * Introduce WalkFunc type
    * Align WalkFunc with path/filepath.WalkFunc
    * Fix Walk usage in find cmd

--- walk.go
+++ walk.go
@@ -1,42 +1,39 @@
 package doozer
 
-type Visitor interface {
-	VisitDir(path string, f *FileInfo) bool
-	VisitFile(path string, f *FileInfo)
-}
+import (
+	"path/filepath"
+)
 
 // Walk walks the file tree in revision rev, rooted at root,
 // analogously to Walk in package path/filepath.
-func Walk(c *Conn, rev int64, root string, v Visitor, errors chan<- error) {
+func Walk(c *Conn, rev int64, root string, v WalkFunc) {
 	f, err := c.Statinfo(rev, root)
 	if err != nil {
-		if errors != nil {
-			errors <- err
-		}
+		v(root, f, err)
 		return
 	}
-	walk(c, rev, root, f, v, errors)
+	walk(c, rev, root, f, v)
 }
 
-func walk(c *Conn, r int64, path string, f *FileInfo, v Visitor, errors chan<- error) {
+func walk(c *Conn, r int64, path string, f *FileInfo, v WalkFunc) {
 	if !f.IsDir {
-		v.VisitFile(path, f)
+		v(path, f, nil)
 		return
 	}
 
-	if !v.VisitDir(path, f) {
+	if v(path, f, nil) == filepath.SkipDir {
 		return
 	}
 
 	list, err := c.Getdirinfo(path, r, 0, -1)
-	if err != nil && errors != nil {
-		errors <- err
+	if err != nil {
+		v(path, f, err)
 	}
 
 	if path != "/" {
 		path += "/"
 	}
 	for i := range list {
-		walk(c, r, path+list[i].Name, &list[i], v, errors)
+		walk(c, r, path+list[i].Name, &list[i], v)
 	}
 }
