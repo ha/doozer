634a7322f81e2e678a8564d5f96b1833cb029c0e
Alexander Simmerl <alx@soundcloud.com>
2012-01-24 22:00:40 +0100

    Change Walk to match filepath.Walk

--- walk.go
+++ walk.go
@@ -6,34 +6,41 @@ import (
 
 // Walk walks the file tree in revision rev, rooted at root,
 // analogously to Walk in package path/filepath.
-func Walk(c *Conn, rev int64, root string, v WalkFunc) {
+func Walk(c *Conn, rev int64, root string, v WalkFunc) error {
 	f, err := c.Statinfo(rev, root)
 	if err != nil {
 		v(root, f, err)
-		return
+		return err
 	}
-	walk(c, rev, root, f, v)
+	return walk(c, rev, root, f, v)
 }
 
-func walk(c *Conn, r int64, path string, f *FileInfo, v WalkFunc) {
-	if !f.IsDir {
-		v(path, f, nil)
+func walk(c *Conn, r int64, path string, f *FileInfo, v WalkFunc) (err error) {
+	verr := v(path, f, nil)
+	if !f.IsDir || verr == filepath.SkipDir {
 		return
 	}
-
-	if v(path, f, nil) == filepath.SkipDir {
-		return
+	if verr != nil {
+		return err
 	}
 
 	list, err := c.Getdirinfo(path, r, 0, -1)
 	if err != nil {
-		v(path, f, err)
+		verr = v(path, f, err)
+		if verr != nil {
+			return err
+		}
 	}
 
 	if path != "/" {
 		path += "/"
 	}
 	for i := range list {
-		walk(c, r, path+list[i].Name, &list[i], v)
+		err = walk(c, r, path+list[i].Name, &list[i], v)
+		if err != nil {
+			break
+		}
 	}
+
+	return
 }
