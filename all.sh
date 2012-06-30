75ed935519322da5862c5b7df5d62ca4658639de
Aram Hăvărneanu <aram@mgk.ro>
2012-06-30 20:13:25 +0200

    doozer: remove vestigial stuff.

--- all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-ver=$(./version.sh)
-printf 'package main\n\nconst version = `%s`\n' "$ver" > cmd/doozer/version.go
-go get -d -v . ./...
-go install -v . ./...
--- clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-go clean . ./...
-rm -rf cmd/doozer/version.go 2>&1 >/dev/null
